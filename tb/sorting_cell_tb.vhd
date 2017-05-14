0a07b6379face59acb03bdb7cc3dde0b6a7a7db1
Michael Mattioli <mmattioli@users.noreply.github.com>
2017-05-14 16:54:47 -0400

    Use `data_width` for sorting_cell_tb bus widths
    Use the generic value for bus widths in sorting_cell testbench instead
    of static values.
    
    Minor whitespace improvements in sorting_cell testbench.

--- tb/sorting_cell_tb.vhd
+++ tb/sorting_cell_tb.vhd
@@ -15,6 +15,7 @@ end sorting_cell_tb;
 architecture behavioral of sorting_cell_tb is
 
     component sorting_cell
+
         generic (data_width : integer := 8);
 
         port (  clk                 : in std_logic;
@@ -26,18 +27,18 @@ architecture behavioral of sorting_cell_tb is
                 next_cell_data      : out std_logic_vector (data_width-1 downto 0);
                 next_cell_occupied  : out boolean;
                 next_cell_push      : out boolean);
+
     end component;
 
     constant clk_period : time := 10 ns;
-    signal end_sim      : boolean := false;
 
     signal clk                  : std_logic := '0';
     signal rst                  : std_logic := '0';
-    signal new_data             : std_logic_vector (7 downto 0) := (others => '0');
-    signal prev_cell_data       : std_logic_vector (7 downto 0) := (others => '0');
+    signal new_data             : std_logic_vector (data_width-1 downto 0) := (others => '0');
+    signal prev_cell_data       : std_logic_vector (data_width-1 downto 0) := (others => '0');
     signal prev_cell_occupied   : boolean := false;
     signal prev_cell_push       : boolean := false;
-    signal next_cell_data       : std_logic_vector (7 downto 0) := (others => '0');
+    signal next_cell_data       : std_logic_vector (data_width-1 downto 0) := (others => '0');
     signal next_cell_occupied   : boolean := false;
     signal next_cell_push       : boolean := false;
 
